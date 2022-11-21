9162d416bc1a81a6511ecaa0c77e142211dc34db
Fabien Fiette <fabienfiette@hotmail.fr>
2022-11-21 19:13:05 +0100

    feat(Header): Implement an appbar on all page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import './App.scss';
 
+import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import './App.scss';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+
+import Header from './components/Header/Header';
 
 function App() {
   const { message } = useSelector((state) => state.init);
 
   return (
-    <Routes>
-      <Route path="/" element={<h1>{message}</h1>} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <>
+      <CssBaseline />
+      <Header />
+      <Routes>
+        <Route path="/" element={<h1>{message}</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 }
 
