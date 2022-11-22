9e9d94bbcc7ec22d69e9e038a41bd72c71e9ab0a
Rémi Rubis <remi.rubis@yahoo.com>
2022-11-22 15:57:48 +0100

    refactor(app): externalise router and use reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import './App.scss';
 
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { CssBaseline } from '@mui/material';
 
-import Header from './components/Header/Header';
+import Header from './Components/Sections/Header/Header';
+import Router from './Router';
 
-function App() {
-  const { message } = useSelector((state) => state.init);
-
-  return (
-    <>
-      <CssBaseline />
-      <Header />
-      <Routes>
-        <Route path="/" element={<h1>{message}</h1>} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <Router />
+    {/* <Footer /> */}
+  </>
+);
 
 export default App;
