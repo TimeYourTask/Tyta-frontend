4febb12a4a53b1bf708c273ba3f68bae55f9325e
Rémi Rubis <remi.rubis@yahoo.com>
2022-12-10 15:04:10 +0100

    feat(ButtonLink): create ButtonLink component to navigate with basic MUI button with react router

--- src/themes/light.js
+++ src/themes/light.js
@@ -1,5 +1,19 @@
+import ButtonLink from '../components/Elements/ButtonLink';
+
 const theme = {
   palette: {},
+  components: {
+    MuiLink: {
+      defaultProps: {
+        component: ButtonLink,
+      },
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: ButtonLink,
+      },
+    },
+  },
 };
 
 export default theme;
