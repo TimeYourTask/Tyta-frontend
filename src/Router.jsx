6e49106fe90f231b48cbad10c2b12431c0671b6a
Rémi Rubis <remi.rubis@yahoo.com>
2022-12-23 16:57:05 +0100

    refactor(Router): change route for adding user in a team

--- src/Router.jsx
+++ src/Router.jsx
@@ -39,7 +39,7 @@ const Router = () => {
       children: [
         { path: 'overview', element: <Overview /> },
         { path: 'teams', element: <ListTeams /> },
-        { path: 'user/:teamID', element: <AddUserToTeam /> },
+        { path: 'user/:teamID/users', element: <AddUserToTeam /> },
       ],
     },
     { path: '*', element: <Navigate to="/404" replace /> },
