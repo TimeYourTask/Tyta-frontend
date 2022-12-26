151c64bbae5da3010e51b7a243712ca15cac1c3e
Rémi Rubis <remi.rubis@yahoo.com>
2022-12-26 12:36:43 +0100

    feat(project.service): adding axios request to fetch data from API

---
+++ src/store/services/projects.service.js
@@ -0,0 +1,14 @@
+import axiosInstance from './axios.config';
+
+const getProjectsAsAdmin = () => axiosInstance.get('/admin/projects').then((res) => res.data);
+
+const getUserProjects = (userId, teamId) => axiosInstance.get(`/user/${userId}/projects`, teamId ? { team: teamId } : null).then((res) => res.data);
+
+const createProject = (teamId, data) => axiosInstance.post(`/team/${teamId}/project`, data)
+  .then((res) => res.data);
+
+export default {
+  getProjectsAsAdmin,
+  getUserProjects,
+  createProject,
+};
