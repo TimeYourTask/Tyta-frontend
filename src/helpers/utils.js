edbd54b90fb22b98cb5c0dfbc4994b903add3533
Fabien <fabienfiette@hotmail.fr>
2023-01-02 18:18:37 +0100

    feat(date): Format date for timer view

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -12,7 +12,10 @@ const formatName = (user) => {
   return name;
 };
 
+const formatDate = (date) => `${(`0${date.getUTCDate()}`).slice(-2)}/${(`0${date.getUTCMonth() + 1}`).slice(-2)}/${date.getUTCFullYear()} ${(`0${date.getUTCHours()}`).slice(-2)}:${(`0${date.getUTCMinutes()}`).slice(-2)}:${(`0${date.getUTCSeconds()}`).slice(-2)}`;
+
 export {
   capitalize,
   formatName,
+  formatDate,
 };
