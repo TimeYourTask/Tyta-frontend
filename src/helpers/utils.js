23713190f1cfe84e689137cc50e6ea6bd4f441d1
Rémi Rubis <remi.rubis@yahoo.com>
2023-01-03 16:16:57 +0100

    refactor(utils): implement timing calculation

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -12,6 +12,19 @@ const formatName = (user) => {
   return name;
 };
 
+const formatDate = (date) => `${(`0${date.getUTCDate()}`).slice(-2)}/${(`0${date.getUTCMonth() + 1}`).slice(-2)}/${date.getUTCFullYear()} ${(`0${date.getUTCHours()}`).slice(-2)}:${(`0${date.getUTCMinutes()}`).slice(-2)}:${(`0${date.getUTCSeconds()}`).slice(-2)}`;
+
+function timeDifference(date1, date2) {
+  const diff = Math.abs(date1 - date2);
+  const secs = Math.floor(diff / 1000);
+
+  const hours = Math.floor(secs / 3600);
+  const mins = Math.floor((secs % 3600) / 60);
+  const remainingSecs = secs % 60;
+
+  return `${hours}h ${mins}m ${remainingSecs}s`;
+}
+
 const normalizeTime = (num) => num.toString().padStart(2, '0');
 
 const convertMsToTime = (milliseconds) => {
@@ -31,5 +44,7 @@ const convertMsToTime = (milliseconds) => {
 export {
   capitalize,
   formatName,
+  formatDate,
+  timeDifference,
   convertMsToTime,
 };
