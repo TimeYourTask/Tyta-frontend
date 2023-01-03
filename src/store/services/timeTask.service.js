cb38ca1a2856b39f40793d0672ffb806c52e8152
Rémi Rubis <remi.rubis@yahoo.com>
2023-01-03 16:20:30 +0100

    feat(TimeTask.service): add start and end function to interract with backend

--- src/store/services/timeTask.service.js
+++ src/store/services/timeTask.service.js
@@ -2,6 +2,12 @@ import axiosInstance from './axios.config';
 
 const getUserTimer = () => axiosInstance.get('/task/user/timer').then((res) => res.data);
 
+const startTimer = (taskId) => axiosInstance.get(`/task/${taskId}/start`).then((res) => res.data);
+
+const endTimer = (taskId) => axiosInstance.get(`/task/${taskId}/end`).then((res) => res.data);
+
 export default {
   getUserTimer,
+  startTimer,
+  endTimer,
 };
