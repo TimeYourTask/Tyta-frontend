135f3f160f852a92b74773d0099f191843ede0c4
Rémi Rubis <remi.rubis@yahoo.com>
2023-01-03 15:49:34 +0100

    refactor(utils): add time conversion function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -12,22 +12,24 @@ const formatName = (user) => {
   return name;
 };
 
-const formatDate = (date) => `${(`0${date.getUTCDate()}`).slice(-2)}/${(`0${date.getUTCMonth() + 1}`).slice(-2)}/${date.getUTCFullYear()} ${(`0${date.getUTCHours()}`).slice(-2)}:${(`0${date.getUTCMinutes()}`).slice(-2)}:${(`0${date.getUTCSeconds()}`).slice(-2)}`;
+const normalizeTime = (num) => num.toString().padStart(2, '0');
 
-function timeDifference(date1, date2) {
-  const diff = Math.abs(date1 - date2);
-  const secs = Math.floor(diff / 1000);
+const convertMsToTime = (milliseconds) => {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
 
-  const hours = Math.floor(secs / 3600);
-  const mins = Math.floor((secs % 3600) / 60);
-  const remainingSecs = secs % 60;
+  seconds %= 60;
+  minutes %= 60;
+  hours %= 24;
 
-  return `${hours}h ${mins}m ${remainingSecs}s`;
-}
+  return `${normalizeTime(hours)}:${normalizeTime(minutes)}:${normalizeTime(
+    seconds
+  )}`;
+};
 
 export {
   capitalize,
   formatName,
-  formatDate,
-  timeDifference,
+  convertMsToTime,
 };
